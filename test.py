b6a55999cd0f6ff6a7d69b7eb59e859d415b275f
Martine Lenders <mail@martine-lenders.eu>
2014-08-12 23:38:26 +0200

    Add test.py with old-formatting test

---
+++ test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Martine Lenders <mail@martine-lenders.eu>
+#
+# Distributed under terms of the MIT license.
+
+"%s" % "test"
+"%d" % 2
+"%.4f" % 2.0
+
